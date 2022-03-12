d304a4a487dc76a081001248783efe008a3ecb16
Pricopi Bianca <pb31627@gmail.com>
2022-03-12 20:02:53 +0200

    user input only string

--- tutorial1/tutorial1/Program.cs
+++ tutorial1/tutorial1/Program.cs
@@ -5,26 +5,13 @@ namespace tutorial1
     {
         static void Main(string[] args)
         {
-            double a = 3.14;
-            int b = Convert.ToInt32(a);
-            Console.WriteLine(a.GetType());
-            Console.WriteLine(b.GetType());
+            Console.Write("Please enter your name: ");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
 
-            int c = 123;
-            double d = Convert.ToDouble(c) + 0.1;
-            Console.WriteLine(d);
-
-            int e = 321;
-            String f = Convert.ToString(e) + "uuaua";
-            Console.WriteLine(f);
-
-            String g = "$";
-            char h = Convert.ToChar(g);
-            Console.WriteLine(h);
-
-            String i = "true";
-            bool j = Convert.ToBoolean(i);
-            Console.WriteLine(j);
+            Console.Write("Please enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You are " + age + " years old.");
         }
     }
 }
