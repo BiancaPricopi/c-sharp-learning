f7845e3d9d8964578898027f9bdb9cadb7c06b75
Pricopi Bianca <pb31627@gmail.com>
2022-03-12 19:50:54 +0200

    constant=const keyword

--- tutorial1/tutorial1/Program.cs
+++ tutorial1/tutorial1/Program.cs
@@ -5,27 +5,8 @@ namespace tutorial1
     {
         static void Main(string[] args)
         {
-            int num;
-            num = 12;
-            Console.WriteLine(num);
-
-            int age = 20;
-            Console.WriteLine("Your age is: " + age);
-
-            double height = 300.5;
-            Console.WriteLine("Your height is: " + height + "cm");
-
-            bool alive = true;
-            Console.WriteLine("Are you alive? " + alive);
-
-            char symbol = '@';
-            Console.WriteLine("Your symbol is: " + symbol);
-
-            String name = "bro";
-            Console.WriteLine("Hello, " + name);
-
-            String username = symbol + "pumpkin";
-            Console.WriteLine("Your usrname is: " + username);
+            const double pi = 3.14159;
+            Console.WriteLine(pi);
 
         }
     }
