c0e95f78abd7e7fedd1c8952964646ab8293b282
Pricopi Bianca <pb31627@gmail.com>
2022-03-25 12:28:26 +0200

    AddTwoNums - accepted

--- DayTwo/AddTwoNums/Program.cs
+++ DayTwo/AddTwoNums/Program.cs
@@ -29,22 +29,44 @@ public class ListNode {
 }*/
 public class Solution
 {
+    public int sizeOfList(ListNode head)
+    {
+        int size = 0;
+        ListNode parcurg = head;
+        while (parcurg.next != null)
+        {
+            parcurg = parcurg.next;
+            size++;
+        }
+        size++;
+        return size;
+    }
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
         ListNode result = new ListNode(-1);
         int sum;
         int remainder = 0;
-        ListNode parcurgL1 = l1;
-        ListNode parcurgL2 = l2;
+        ListNode parcurgL1;
+        ListNode parcurgL2;
+        if (sizeOfList(l1) > sizeOfList(l2))
+        {
+            parcurgL1 = l1;
+            parcurgL2 = l2;
+        }
+        else
+        {
+            parcurgL1 = l2;
+            parcurgL2 = l1;
+        }
         bool finishL2 = false;
         bool finishL1 = false;
         int val2;
-        while(parcurgL1.next != null && !finishL1)
+        while(parcurgL1.next != null || !finishL1)
         {
             if (parcurgL2.next != null)
             {
                 val2 = parcurgL2.val;
-                parcurgL2 = parcurgL1.next;
+                parcurgL2 = parcurgL2.next;
             }
             else
             {
@@ -59,6 +81,7 @@ public class Solution
                 }
             }
             sum = parcurgL1.val + val2 + remainder;
+            remainder = 0;
             if(sum >= 10)
             {
                 remainder = sum / 10;
@@ -85,6 +108,15 @@ public class Solution
                 parcurgL1 = parcurgL1.next;
             }
         }
+        if(remainder > 0)
+        {
+            ListNode parcurg = result;
+            while (parcurg.next != null)
+            {
+                parcurg = parcurg.next;
+            }
+            parcurg.next = new ListNode(remainder % 10);
+        }
 
         return result;
     }
@@ -95,15 +127,15 @@ public class Test
     public static void Main()
     {
         Solution solution = new Solution();
-        ListNode l1 = new ListNode(2);
-        ListNode n1 = new ListNode(4);
-        ListNode n2 = new ListNode(3);
+        ListNode l1 = new ListNode(9);
+        ListNode n1 = new ListNode(9);
+        ListNode n2 = new ListNode(9);
         l1.next = n1;
         n1.next = n2;
 
-        ListNode l2 = new ListNode(5);
-        n1 = new ListNode(6);
-        n2 = new ListNode(4);
+        ListNode l2 = new ListNode(9);
+        n1 = new ListNode(9);
+        n2 = new ListNode(9);
         l2.next = n1;
         n1.next = n2;
 
