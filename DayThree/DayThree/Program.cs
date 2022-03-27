dce53d82bc78b5f9b844b7f930ec690f244f36ab
Pricopi Bianca <pb31627@gmail.com>
2022-03-27 12:00:18 +0300

    "solution class and test"

--- DayThree/DayThree/Program.cs
+++ DayThree/DayThree/Program.cs
@@ -2,6 +2,36 @@
 {
     public int LengthOfLongestSubstring(string s)
     {
+        int substringLenght = 0;
+        int maxSubstringLength = 0;
+        List<char> list = new List<char>();
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(!list.Contains(s[i]))
+            {
+                list.Add(s[i]);
+                substringLenght++;
+            }
+            else
+            {
+                if(substringLenght > maxSubstringLength)
+                {
+                    maxSubstringLength = substringLenght;
+                }
+                substringLenght = 0;
+                list.Clear();
+            }
+        }
+        return maxSubstringLength;
+    }
+}
 
+public class Test
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        int l = solution.LengthOfLongestSubstring("pwwkew");
+        Console.WriteLine("length = " + l);
     }
 }
