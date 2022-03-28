315afc5447d9b8031f5aa9482300422730c6e388
Pricopi Bianca <pb31627@gmail.com>
2022-03-28 10:15:16 +0300

    "working"

--- DayFour/Palindrome/Program.cs
+++ DayFour/Palindrome/Program.cs
@@ -2,6 +2,34 @@
 {
     public bool IsPalindrome(int x)
     {
+        int num = 0;
+        int copy = x;
+        int pow = 1;
+        int numOfDigits = 1;
+        while(copy > 0)
+        {
+            numOfDigits *= 10;
+            copy /= 10;
+        }
+        numOfDigits /= 10;
+        copy = x;
+        while (copy > 0)
+        {
+            int digit = copy % 10;
 
+            num += numOfDigits * digit;
+            numOfDigits /= 10;
+            copy /= 10;
+        }
+        return num == x;
+    }
+}
+
+public class Test
+{
+    public static void Main()
+    {
+        Solution solution = new Solution();
+        Console.WriteLine(solution.IsPalindrome(555));
     }
 }
